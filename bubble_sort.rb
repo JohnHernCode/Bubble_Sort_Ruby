c761a0f0b6b0d9f3656773fcb02b9f0b0763561e
“JohnHernCode” <besthostandseoinfo@gmail.com>
2020-12-22 16:08:13 -0600

    Fix Linters for Ruby

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,11 +3,11 @@ def bubble_sort(array)
     swapped = false # indicator for when to stop the loop
     (array.length - 1).times do |i|
       if array[i] > array[i + 1]
-         array[i], array[i + 1] = array[i+1], array[i]
-            swapped = true
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
       end
     end
-    break if not swapped
+    break unless swapped
   end
   array
 end
@@ -15,18 +15,13 @@ end
 def bubble_sort_by(array)
   loop do
     swapped = false
-    (array.length-1).times do |i|
-    if yield(array[i], array[i + 1]) > 0
+    (array.length - 1).times do |i|
+      if yield(array[i], array[i + 1]) > 0
         array[i], array[i + 1] = array[i + 1], array[i]
-            swapped = true
+        swapped = true
+      end
     end
-    end
-    break if not swapped
+    break unless swapped
   end
   array
 end
-
-print (
-  bubble_sort_by(["hello", "hey", "hi"]) do |left, right|
-    left.length - right.length
-  end)
